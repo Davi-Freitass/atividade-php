f4bc40735651908052f1dc560938015b89d170e5
Davi-Freitass <davi_f_freitas@estudante.sesisenai.org.br>
2026-07-09 11:44:03 -0300

    ex03

--- ex03.php
+++ ex03.php
@@ -3,7 +3,7 @@
     function mascararCpf($cpf)
     {
 
-        return substr_replace($cpf, '***.***.', 0, 7);
+        return substr_replace($cpf, '***.***.', 0, 9);
     }
 
     echo "CPF mascarado: " . mascararCpf("123.456.789-00");
