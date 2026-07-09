7a741625b679f92a3639f3cf045f604dafc50619
Davi-Freitass <davi_f_freitas@estudante.sesisenai.org.br>
2026-07-09 12:41:09 -0300

    add

---
+++ ex04.php
@@ -0,0 +1,21 @@
+<?php
+
+function gerarSenha($tamanho = 8)
+{
+    $numeros = '0123456789';
+    $letras = 'abcdefghijklmnopqrstuvwxyz';
+    $letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $especiais = '!@#$%^&*()_+-=';
+    
+
+    $senha .= $numeros[rand(0, strlen($numeros) - 1)];
+    $senha .= $letras[rand(0, strlen($letras) - 1)];
+    $senha .= $letrasMaiusculas[rand(0, strlen($letrasMaiusculas) - 1)];     
+    $senha .= $especiais[rand(0, strlen($especiais) - 1)];  
+
+    $senha = str_shuffle($senha);
+
+    return $senha;
+}
+
+echo "Senha gerada: " . gerarSenha(8);
